2e4f968624cfb4b2a2f977abd924712026d4b11f
Nico Heinola <nico.heinola@gmail.com>
2024-03-05 21:34:07 +0200

    hexagon image

--- src/views/FrontPage.js
+++ src/views/FrontPage.js
@@ -3,13 +3,20 @@ import EncryptedCursorText from "components/text/EncryptedCursorText";
 import "styles/views/frontpage.scss"
 
 function FrontPage() {
-    let description = "I'm a passionate programmer with motivation to constantly learn something new. I specialize in web development, but I'm also proficient in making desktop applications or games using Unity and Godot 4.";
+    let description = "I'm a passionate programmer with motivation to constantly learn something new. I specialize in web development but I'm also proficient at making desktop applications and games.";
     return (
         <div className="front-page">
             <div className="hi-section">
-                <CursorText fontSize="big" name={[{ text: "", waitSeconds: 0.70, speed: 0 }, { text: "Hi,", waitSeconds: 1, speed: 0.05 }, { text: " I'm Nico!", waitSeconds: 0, speed: 0.05 }]}></CursorText>
-                <EncryptedCursorText decryptionCount={8} decryptionDelaySeconds={0.05} encryptionDelaySeconds={0.05} delaySeconds={0} fontSize="small" name={[{ text: "", waitSeconds: 3.4, speed: 0 }, { text: description, waitSeconds: 0, speed: 0.008 }]}>
-                </EncryptedCursorText>
+                <div className="about-me">
+                    <CursorText className="title" fontSize="big" name={[{ text: "", waitSeconds: 0.70, speed: 0 }, { text: "Hi,", waitSeconds: 1, speed: 0.05 }, { text: " I'm Nico!", waitSeconds: 0, speed: 0.05 }]}></CursorText>
+                    <EncryptedCursorText decryptionCount={8} decryptionDelaySeconds={0.03} encryptionDelaySeconds={0.05} delaySeconds={0} fontSize="small" name={[{ text: "", waitSeconds: 3, speed: 0 }, { text: description, waitSeconds: 0, speed: 0.006 }]}>
+                    </EncryptedCursorText>
+                </div>
+            </div>
+            <div className="image-section">
+                <div className="image-container">
+                    <img src="images/me.jpg" />
+                </div>
             </div>
         </div>
     );
