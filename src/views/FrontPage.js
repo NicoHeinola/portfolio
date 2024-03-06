f232c980397206a5e205ccb887e57cd081990558
Nico Heinola <nico.heinola@gmail.com>
2024-03-06 21:34:53 +0200

    quick changes. todo: make chrome check work

--- src/views/FrontPage.js
+++ src/views/FrontPage.js
@@ -2,7 +2,9 @@ import CursorText from "components/text/CursorText";
 import EncryptedCursorText from "components/text/EncryptedCursorText";
 import "styles/views/frontpage.scss"
 
-function FrontPage() {
+function FrontPage() {// FIX THIS CHROME CHEKC
+    const isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+
     let description = "I'm a passionate programmer with motivation to constantly learn something new. I specialize in web development but I'm also proficient at making desktop applications and games.";
     return (
         <div className="front-page">
@@ -10,20 +12,20 @@ function FrontPage() {
             <div className="hi-section">
                 <div className="social-medias">
                     <div className="list-of-medias">
-                        <a className="media" style={{ animationDelay: ".5s" }} href="https://github.com/NicoHeinola" target="_blank">
+                        <a className="media" style={{ animationDelay: ".5s" }} href="https://github.com/NicoHeinola" target="_blank" rel="noreferrer">
                             <img className="icon" src="icons/github.png" />
                         </a>
-                        <a className="media" style={{ animationDelay: ".6s" }} href="https://github.com/NicoHeinola" target="_blank">
+                        <a className="media" style={{ animationDelay: ".6s" }} href="https://github.com/NicoHeinola" target="_blank" rel="noreferrer">
                             <img className="icon" src="icons/linkedin.png" />
                         </a>
-                        <a className="media" style={{ animationDelay: ".7s" }} href="https://github.com/NicoHeinola" target="_blank">
+                        <a className="media" style={{ animationDelay: ".7s" }} href="https://github.com/NicoHeinola" target="_blank" rel="noreferrer">
                             <img className="icon" src="icons/mail.png" />
                         </a>
                     </div>
                     <div className="seperator-line" style={{ animationDelay: "0.3s" }}></div>
                 </div>
                 <div className="about-me">
-                    <CursorText className="title" fontSize="big" delaySeconds={.1} name={[{ text: "", waitSeconds: 0.50, speed: 0 }, { text: "Hi,", waitSeconds: 0.55, speed: 0.1 }, { text: " I'm Nico!", waitSeconds: 0, speed: 0.035 }]}></CursorText>
+                    <CursorText className="title" fontSize="big" delaySeconds={isChrome ? 0.5 : 0.1} name={[{ text: "", waitSeconds: 0.50, speed: 0 }, { text: "Hi,", waitSeconds: 0.30, speed: 0.1 }, { text: " I'm Nico!", waitSeconds: 0, speed: 0.035 }]}></CursorText>
                     <EncryptedCursorText decryptionCount={7} delaySeconds={.1} decryptionDelaySeconds={0.03} encryptionDelaySeconds={0.05} fontSize="small" name={[{ text: "", waitSeconds: 1.8, speed: 0 }, { text: description, waitSeconds: 0, speed: 0.006 }]}>
                     </EncryptedCursorText>
                 </div>
